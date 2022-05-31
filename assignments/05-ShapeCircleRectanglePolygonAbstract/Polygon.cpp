ec4de112626c277d79bfbdbb12d26fc2e637ec60
JuanCarlosHR2003 <A01740821@tec.mx>
2022-05-31 14:18:18 +0000

    Poligono abstracto

---
+++ assignments/05-ShapeCircleRectanglePolygonAbstract/Polygon.cpp
@@ -0,0 +1,22 @@
+#include "Polygon.hpp"
+#include <string>
+
+Polygon::Polygon(){
+    n = 0;
+    lado = 0;
+    apotema = 0;
+}
+
+Polygon::Polygon(int ValX, int ValY, int n, int lado, int a) : Shape{ValX, ValY}{
+    this->n = n;
+    this->lado = lado;
+    this->apotema = a;
+}
+
+std::string Polygon::draw(){
+    return "soy un polígono";
+}
+
+double Polygon::getArea(){
+    return n * lado * apotema / 2;
+}
